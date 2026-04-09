acd8bc9262c1831de107dc97c896421a44a5ff8c
Abdul Gani <ganiandesba22@gmail.com>
2026-04-09 14:14:24 +0700

    first project portfolio

---
+++ resources/views/portfolio.blade.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Portfolio Abdul Gani</title>
+    <style>
+        body {
+            font-family: Arial;
+            margin: 0;
+            background: #0f172a;
+            color: white;
+        }
+        header {
+            background: #1e293b;
+            padding: 20px;
+            text-align: center;
+        }
+        section {
+            padding: 40px;
+        }
+        .card {
+            background: #1e293b;
+            padding: 20px;
+            margin: 10px 0;
+            border-radius: 10px;
+        }
+        h1, h2 {
+            color: #38bdf8;
+        }
+        .card {
+    transition: 0.3s;
+}
+.card:hover {
+    transform: scale(1.05);
+}
+    </style>
+</head>
+<body>
+
+<header>
+    <h1>🔥 Abdul Gani</h1>
+    <p>Network Engineer | Sysadmin | Future DevOps</p>
+</header>
+
+<section>
+    <h2>👨‍💻 Tentang Saya</h2>
+    <div class="card">
+        Saya seorang IT enthusiast yang fokus pada networking, server, dan system administration.
+    </div>
+</section>
+
+<section>
+    <h2>🛠️ Skill</h2>
+    <div class="card">
+        - Linux Server (Ubuntu) <br>
+        - Networking (Mikrotik, VLAN) <br>
+        - Web Server (Nginx) <br>
+        - Git & Deployment
+    </div>
+</section>
+
+<section>
+    <h2>🚀 Project</h2>
+    <div class="card">
+        - Lab Proxmox + VLAN <br>
+        - Server Nginx  <br>
+        - Laravel Basic App
+    </div>
+</section>
+
+<section>
+    <h2>📞 Contact</h2>
+    <div class="card">
+        Email: gani@example.com <br>
+        GitHub: github.com/username
+    </div>
+</section>
+
+</body>
+</html>
